77d50ae606192f8b8be1e251809e78f0a1b49c75
Isiyemi Joseph <Josephisiyemi1@gmail.com>
2022-03-30 04:27:07 +0100

    Added space

--- lib/modules/home/views/dashboard.dart
+++ lib/modules/home/views/dashboard.dart
@@ -200,7 +200,7 @@ class _DashboardState extends ConsumerState<Dashboard> {
                 }),
               )),
               const SizedBox(
-                height: 70,
+                height: 100,
               )
             ],
           ),
